173bc109badcf6d08e6e1741d7b6d9bd779339bd
kevinMG01 <112394787+kevinMG01@users.noreply.github.com>
2022-10-31 19:28:29 -0300

    areglamos el movimiento

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -1,27 +1,56 @@
 extends KinematicBody2D
 
-
+var gravity = 100
 var speed = 100
 var velocity = Vector2()
-#var max_velocity = 50
+var jump = -200
+
 
 
 func _ready():
+
+	pass
+
+
+
+func _process(delta):
 	
-	pass 
+	animacion()
 	
+
 func _physics_process(delta):
-	move()
-	move_and_slide(velocity)
+	
+	move(delta)
+	move_and_slide(velocity, Vector2(0, -1))
 
 
-func move():
+func move(delta):
 	velocity.x = 0
+	velocity.y += gravity * delta
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= speed
 		
-	elif Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"):
 		velocity.x += speed
+	
+	#if !is_on_floor():
+	#	velocity.y += gravity * delta
+	
+	if Input.is_action_pressed("ui_up"):
+			velocity.y = jump
+
+func animacion():
+	if velocity.x < 0:
+		$AnimatedSprite.animation = ""
+		
+		$AnimatedSprite.flip_h = true
+	if velocity.x > 0:
+		$AnimatedSprite.animation = ""
+		
+		$AnimatedSprite.flip_h = false
+	if velocity.x == 0:
+		$AnimatedSprite.animation = "Idle"
+	
 	
 
 
