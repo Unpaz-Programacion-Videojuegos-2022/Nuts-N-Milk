c0666db76ac33113f08828cddeaa0d69fc192e3a
kevinMG01 <112394787+kevinMG01@users.noreply.github.com>
2022-11-05 01:55:47 -0300

    menu
    ya funciona el menu

---
+++ scenes/menu_2/menu_2.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+
+var menus = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	$AnimatedSprite.animation = "menu"
+	pass
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		menus += 1
+		if menus == 1:
+			$AnimatedSprite.animation = "creditos"
+		if menus == 2:
+			$AnimatedSprite.animation = "tuto"
+		if menus == 3:
+			get_tree().change_scene("res://scenes/World/World.tscn")
