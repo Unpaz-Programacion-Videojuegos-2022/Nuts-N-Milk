dd8f4840d3582be342af39833a06318d49fca3a4
kevinMG01 <112394787+kevinMG01@users.noreply.github.com>
2022-11-07 02:56:42 -0300

    enemigo
    provamos

--- scenes/Enemy/Enemy.gd
+++ scenes/Enemy/Enemy.gd
@@ -1,5 +1,21 @@
 extends KinematicBody2D
 
+var movimiento = false
+
+var velocity = Vector2()
+var speed = 100
+
+
+
+
+
+
+func _physics_process(delta):
+	if movimiento:
+		move_and_slide(velocity)
+		
+	
+	pass
 
 
 
@@ -8,6 +24,8 @@ func _on_Area2D_body_entered(body):
 	body.get_name()
 	if body.is_in_group("player"):
 		print("hola mundo")
+		movimiento = true
+		
 	pass
 
 		
